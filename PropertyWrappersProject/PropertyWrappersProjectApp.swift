cc547dbf317cb6265eae4922d5e4f8db5d937ed0
Alvaro Guerra <aguerfr@mercadona.es>
2025-03-06 14:51:04 +0100

    Initial Commit

---
+++ PropertyWrappersProject/PropertyWrappersProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersProjectApp.swift
+//  PropertyWrappersProject
+//
+//  Created by Alvaro Guerra Freitas on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
